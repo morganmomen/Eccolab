a6fff4917c76c199dbaf41b8e1e9365d14be0b2f
morganmomen <morganmomen.pro@gmail.com>
2023-10-14 17:01:24 +0200

    Fin acceuil

--- index.php
+++ index.php
@@ -7,31 +7,33 @@
     <meta name="description" content="">
     <meta name="author" content="Morgan MOMEN, Tristan VILLETTE">
       <link rel="stylesheet" href="style/main.css">
+      <link rel="stylesheet" href="style/contact.css">
 <title>Ecollab'</title>
   </head>
   <body>
     
 <header>
     <div id="navbar">
-        <div id="logo" class="container">
-        <a href="index.php" class="navbar-brand d-flex align-items-center">
+        <div id="logo">
+        <a href="index.php">
             <img id="logo_img" src="images/logo.png">
         </a>
 
         </div>
-        <div id="Depot" class="container">
-        <a href="panier.php" class="navbar-brand d-flex align-items-center">
-            <strong>Déposer un produit</strong>
+        <div id="Depot">
+        <a href="panier.php" width=100% heigth=100%>
+            <strong>Je dépose des produits</strong>
         </a>
         </div>
-        <div id="Achat" class="container">
-        <a href="commandes.php" class="navbar-brand d-flex align-items-center">
-            <strong>Commandes</strong>
+        <div id="Achat">
+        <a href="commandes.php">
+            <strong>J'achète des produits</strong>
         </a>
         </div>
-        <div id="Account" class="container">
-        <a href="admin.php" class="navbar-brand d-flex align-items-center">
-            <strong>Account</strong>
+        <div id="Account">
+        <a href="login.php">
+            <img src="images/account.png">
+
         </a>
         </div>
     </div>
@@ -39,10 +41,34 @@
 
 <main>
     <div id="main_container">
+        <div id="title">
+            <h4><strong>ECOLLAB</strong>'</h4>
+        </div>
+        <div id="subtitle">
+            <h3>Réinventons l'avenir en recyclant le passé</h3>
+    </div>
+        <a href=#explanations><div class="arrow"></div></a>
     </div>
 <div id="contacts">
+    <div id="normal-text"><strong>Nous sommes Ecollab'. Contactez nous ! </strong></div>
+    <div id="write-effect">
+        <div class="typed-out">contact@ecollab.com</div>
+        <div class="typed-out">1 Rue de la Chebarde, Aubière</div>
+        <div class="typed-out">09 03 01 05 04</div>
+    </div>
 </div>
     <div id="explanations">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper sed magna at venenatis. Integer rutrum vulputate interdum. Nullam lobortis eros eu mauris varius scelerisque. Ut non magna ultricies sem semper commodo. Vestibulum malesuada turpis quis mauris ornare, in porttitor neque luctus. Nulla tempor quis purus eget iaculis. Duis tristique diam tristique metus blandit, ac rhoncus nisl aliquet. Pellentesque ac risus et urna semper pulvinar. Quisque nec metus in urna congue sollicitudin. Aliquam erat volutpat. Etiam sodales vestibulum diam, vitae ornare erat rutrum quis. Nunc non velit sed nisl porta mollis eu non augue. Suspendisse ut tellus efficitur, volutpat nulla eu, hendrerit nunc. Etiam a dignissim orci. Aliquam id tellus sed nibh consectetur pretium. Donec vitae ex et purus efficitur efficitur quis vel ipsum.
+
+            Nunc sapien ligula, tempus nec condimentum ut, tincidunt sed ex. Nam in leo sit amet eros imperdiet ornare. Morbi hendrerit massa lectus, sed tincidunt ante aliquam id. Donec hendrerit odio ac tortor imperdiet, vitae pulvinar est hendrerit. Phasellus commodo tempus magna, vel euismod velit interdum ut. Phasellus eu fermentum justo, et commodo quam. Donec convallis massa eget enim imperdiet, at laoreet turpis euismod.
+
+            Praesent ut porttitor risus, at dignissim ex. Morbi non lorem aliquet, elementum risus laoreet, feugiat dui. Sed quis eros aliquam, viverra neque non, gravida felis. Pellentesque suscipit elit nec metus porta, sed mattis sapien convallis. Phasellus sollicitudin non quam et hendrerit. Nullam at lacus in dolor rutrum sollicitudin. Donec consequat ipsum enim, id viverra velit pretium at. Sed a aliquam erat. Proin id sem vulputate, facilisis tellus interdum, vestibulum sem.
+
+            Cras ipsum velit, blandit vel volutpat non, molestie nec elit. Vestibulum at metus vel nulla tincidunt consectetur vitae et justo. Mauris condimentum tortor diam, ut sodales tortor lobortis non. Quisque ac pulvinar nunc. Praesent vel molestie dolor, id aliquet nisl. Nam ut erat porta, faucibus nisl et, fermentum neque. Mauris molestie tincidunt massa. Curabitur ut maximus nisl, a cursus mauris.
+
+            Curabitur sagittis, nunc ac eleifend aliquet, neque magna lacinia augue, non vulputate urna augue et turpis. Pellentesque convallis, sem vel pharetra tristique, risus enim accumsan turpis, ut lacinia odio metus non eros. Nullam dictum erat quis sapien interdum tristique. Phasellus sapien orci, accumsan sed ornare eget, egestas quis quam. Maecenas hendrerit aliquet justo, id egestas enim porta vel. Aliquam convallis vitae lectus sed molestie. Nullam ut mollis ipsum, rutrum euismod ligula. Integer auctor scelerisque enim nec eleifend. Phasellus risus leo, viverra id varius a, porta eu lectus. Sed massa massa, lacinia ac augue ac, luctus ultricies mauris. Duis pretium ligula justo. Quisque ac tincidunt dolor. Pellentesque tortor turpis, fringilla eget viverra eget, consectetur sed nulla. Mauris ut ipsum tristique, dapibus dolor vitae, placerat ipsum.
+        </p>
 
     </div>
 </main>
